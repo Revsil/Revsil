7b60c02532ee74cdf6bc7e01052a29d1aff01abc
Sorch <sorch@protonmail.ch>
2022-05-25 16:05:01 +0100

    Because revolt does not shard removal of ShardingManager has been done q-q

--- TestBot.js
+++ TestBot.js
@@ -1,8 +1,8 @@
 let
-	Revsil = require("./"),
+	Revsil = require("./revsil"),
 	util = require("util"),
-	humanize = require("humanize");
-	
+	humanize = require("humanize"),
+	duration = require("humanize-duration");
 
 function codeblock(text) {
 	return `\`\`\`js\n${text}\n\`\`\``;
@@ -11,36 +11,38 @@ function codeblock(text) {
 class TestBot {
 	constructor() {
 		this.revsil = new Revsil("");
-		this.revsil.shards.map(shard => {
-			this.handleRevolt(this.revsil.shards.get(shard.id));
-		})
+		this.revsil.connect();
+		this.handleRevolt(this.revsil.revsil);
+		this._start = Date.now();
+
+
 	}
-	
+
 	canRun(sender) {
 		if([""].includes(sender)) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	handleRevolt(m) {
-		
+		console.log(m);
 		m.on("raw", event => {
 			//console.log("raw", event);
 		});
-		
+
 		m.on("debug", event => {
 			//console.log(event);
 		});
-		
+
 		m.on("onGuildCreate", guild => {
 			//console.log(guild.members);
 		});
-		
+
 		m.on("ClientReady", () => {
 			console.log("i\'m ready");
 		});
-		
+
 		m.on("onMessage", async msg => {
 			if(msg.author.bot) {
 				return;
@@ -52,14 +54,13 @@ class TestBot {
 				let body = msg.content.slice(1).split(" ");
 				let cmd  = body[0];
 				let args = body.slice(1).join(" ");
-				
-				
+
+
 				if(cmd == "rt") {
-					let stamp = process.uptime();
-					let now = Math.floor(Date.now() / 1000);
-					m.createMessage(msg.channel, humanize.relativeTime(now - stamp))
+					let now = Date.now();
+					m.createMessage(msg.channel, duration(now - this._start, { delimiter: ' and ' , round: true}) + " ago ^^");
 				}
-				
+
 				if(cmd == "eval" && this.canRun(msg.author)) {
 					let output;
 					try {
@@ -78,4 +79,3 @@ class TestBot {
 }
 
 x = new TestBot();
-x.revsil.connect();
--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const RevsilHandler = require("./lib");
 function Revsil(token, options) {
     return new RevsilHandler.RevsilHandler(token, options);
 }
-Revsil.VERSION = require('./package.json').version;
+Revsil.VERSION = require('../package.json').version;
 
 module.exports = Revsil;
